f30b9b2e4077e8463de85a02fbcc0b8bec5401d9
Praval Pratap Singh <113175059+Pravalpratap@users.noreply.github.com>
2024-06-10 10:44:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-function getFormvalue() {
-    //Write your code here
+function getFormvalue(event) {
+    // Prevent the form from actually submitting
+    event.preventDefault();
 
+    // Get the values of the first and last name fields
+    let firstName = document.forms["form1"]["fname"].value;
+    let lastName = document.forms["form1"]["lname"].value;
+
+    // Display the values in an alert
+    alert(firstName + " " + lastName);
 }
